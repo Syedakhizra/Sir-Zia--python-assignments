94aff583e25f99cf875ae523ae2762f34b833871
Syedakhizra <159184686+Syedakhizra@users.noreply.github.com>
2025-05-24 04:02:42 +0500

    Add files via upload

---
+++ Password Strength Meter/main.py
@@ -0,0 +1,61 @@
+import streamlit as st
+
+def check_password(password):
+    score = 0
+    tips = []
+
+    if len(password) >= 8:
+        score += 1
+    else:
+        tips.append("🔴 Use at least 8 characters...")
+
+    if any(c.isupper() for c in password):
+        score += 1
+    else:
+        tips.append("🟠 Include upper letter...")
+
+    if any(c.islower() for c in password):
+        score += 1
+    else:
+        tips.append("🟡 Include lower letter...")
+
+    if any(c.isdigit() for c in password):
+        score += 1
+    else:
+        tips.append("🟢 Add a number (0-9)...")
+    
+    if any(c in "!@#$%^&" for c in password):
+        score += 1
+    else:
+        tips.append("🔵 Use a special character (!@#$%^&)... ")
+
+
+
+    return score , tips
+    
+
+def main():
+    st.title("🔐Password Strength Meter:")
+    password = st.text_input("Enter Password 🔑", type = "password")
+
+
+    score = 0
+    tips = []
+
+
+    if password:
+        score , tips = check_password(password)
+
+    if score == 5:
+        st.success("✅Strong Password! Secure and Safe..")
+    elif score ==3 or score == 4:
+        st.warning("⚠️ Moderate Password! Improve it...")
+    else:
+        st.error("❌Weak Password! Follow these steps:")
+
+        
+        for tip in tips:
+          st.write(tip)
+
+
+main()
